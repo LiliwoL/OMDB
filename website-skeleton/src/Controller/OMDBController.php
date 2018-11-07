39d51024cbdb8178b2e56d8d3da8f67b58dc8a88
LiliwoL <github@liliwol.fr>
2018-11-07 09:25:22 +0100

    Last mofifs

--- website-skeleton/src/Controller/OMDBController.php
+++ website-skeleton/src/Controller/OMDBController.php
@@ -2,8 +2,11 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class OMDBController extends AbstractController
 {
@@ -82,6 +85,89 @@ class OMDBController extends AbstractController
             array(
                 'query' => $query,
                 'movies' => $json->Search   // On ne transmet que la propriété Search qui contient les films
+            )
+        );
+    }
+
+
+    /**
+     * @Route(
+     *  "/film_avec_parametre_et_avec_formulaire/{query}",
+     *  name="Film avec paramètre et avec formulaire",
+     *  defaults={
+     *      "query" = "cité"
+     *  }
+     * )
+     */
+    public function affichageFilmAvecParametreEtAvecFormulaire( $query )
+    {
+        $apiKey = '185a318e';
+        //$query = "running";
+
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, 'http://www.omdbapi.com/?s=' . $query . '&apikey=' . $apiKey);
+        curl_setopt($ch,  CURLOPT_RETURNTRANSFER, true);
+
+        $resultat_curl = curl_exec($ch);
+
+        // On transforme le résultat de cURL en un objet JSON utilisable
+        $json = json_decode ( $resultat_curl );
+
+        // Création d'un formulaire simple
+        $form = $this->createFormBuilder()
+            ->add('query', TextType::class)
+            ->add('save', SubmitType::class, array('label' => 'Rechercher'))
+            ->getForm();
+
+
+        // Rendu
+        return $this->render('omdb/formulaire.html.twig', 
+            array(
+                'query' => $query,
+                'movies' => $json->Search,   // On ne transmet que la propriété Search qui contient les films
+                'form' => $form->createView()
+            )
+        );
+    }
+
+
+    /**
+     * @Route(
+     *  "/film_avec_parametre_et_avec_formulaire/",
+     *  name="Film avec paramètre et avec formulaire en POST"
+     * )
+     */
+    public function affichageFilmAvecParametreEtAvecFormulairePOST( Request $request )
+    {
+        $apiKey = '185a318e';
+
+        if ( $request->request->get('query') )
+        {
+            $query = $request->request->get('query');
+        }else{
+            $query = "rien";
+        }
+
+    
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, 'http://www.omdbapi.com/?s=' . $query . '&apikey=' . $apiKey);
+        curl_setopt($ch,  CURLOPT_RETURNTRANSFER, true);
+
+        $resultat_curl = curl_exec($ch);
+
+        // On transforme le résultat de cURL en un objet JSON utilisable
+        $json = json_decode ( $resultat_curl );
+
+
+
+        // Rendu
+        return $this->render('omdb/formulaire.html.twig', 
+            array(
+                'query' => $query,
+                'movies' => $json->Search,   // On ne transmet que la propriété Search qui contient les films
+                'form' => $form->createView()
             )
         );
     }
